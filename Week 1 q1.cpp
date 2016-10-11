307c092c7c524bcad0af5d550eda0e55af01ef65
GavinOSHC <dhaliw30@uni.coventry.ac.uk>
2016-10-11 13:21:29 +0100

    Create Week 1 q1.cpp

---
+++ Week 1 q1.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <array>
+#include <ctime>
+#include <cstdlib>
+using namespace std;
+
+int main(){
+    
+    //creating the array
+    array<int, 8> newArray = {5,3,8,6,1,9,2,7};
+    
+    //seeding the random numbers
+    srand(time(0));
+    
+    //looping thorugh the array 
+    for(int x = 0; x < 8; x++){
+        
+        
+        //generate a random number  
+        int random = rand() % 8 + 0;
+        
+        //stop the repeating by switch newArray[x] with newArray[random]
+        int rep = newArray[x];
+        newArray[x] = newArray[random];
+        newArray[random] = rep;
+        
+    }
+    
+    //looping thorugh the array printing out the values
+    for (int x = 0; x < 8; x++){
+        cout << newArray[x];
+        
+    }
+    
+    return 0;
+}
