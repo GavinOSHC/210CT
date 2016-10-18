e974b94b56a249d8ee506aba965e720a2da97c10
GavinOSHC <dhaliw30@uni.coventry.ac.uk>
2016-10-18 14:23:09 +0100

    Update Week 2 q1.cpp

--- Week 2 q1.cpp
+++ Week 2 q1.cpp
@@ -19,3 +19,46 @@ HPS(n):
     print ceil_n_squard
   
   
+Code in C++
+
+#include<iostream>
+#include<math.h>
+using namespace std;
+
+
+int HPS(int n){
+    
+    int root = sqrt(n); 
+    
+    int floor_n = int(root);
+    int ceil_n = floor_n + 1;    //Creat a ceil int
+    
+    
+    // square both numbers 
+    int floor_n_sqaure = floor_n * floor_n;
+    int ceil_n_square = ceil_n * ceil_n; 
+    
+    // calculate square closest to the orginal
+    int floor_gap = n - floor_n_sqaure;
+    int ceil_gap = ceil_n_square - n;
+    
+    if (floor_gap < ceil_gap)
+        cout << floor_n_sqaure;
+        
+    else 
+        cout <<  ceil_n_square;
+        
+    
+    
+}
+
+
+int main(){
+    
+    int n = 0;
+    cout << "Please enter a number: " << endl;
+    cin >> n; 
+    HPS(n);
+    return 0;
+    
+}
