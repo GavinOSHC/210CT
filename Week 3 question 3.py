5f9abb9289e6c2239c5159955a91548fe662a9e3
GavinOSHC <dhaliw30@uni.coventry.ac.uk>
2016-10-27 10:33:36 +0100

    Update Week 3 question 3.py

--- Week 3 question 3.py
+++ Week 3 question 3.py
@@ -5,7 +5,9 @@ def remove_vowels(s):
         return s
     elif s[0] in "aeiouAEIOU":      #first character is a vowel
         return remove_vowels(s[1:]) #skip first letter
-    return s[0] + remove_vowels(s[1:]) #return the first letter 
+    else:
+        return s[0] + remove_vowels(s[1:]) #return the first letter 
+
 
 s = "beautiful"
 print(remove_vowels(s))
