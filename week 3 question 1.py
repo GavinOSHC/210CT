1460d119644fbbe94d4aafaba3fa428065c75246
GavinOSHC <dhaliw30@uni.coventry.ac.uk>
2016-10-28 19:33:56 +0100

    Update week 3 question 1.py

--- week 3 question 1.py
+++ week 3 question 1.py
@@ -1,7 +1,22 @@
+psuedocode 
+
+string <- enter sentance
+string <- string.split()
+
+STRINGREV(string):
+    stringLength <- length(string)
+    if stringLength equal to 1:
+        return string
+    else:
+        return[string[-1]] + STRINGREV(string[:-1])
+    
+string <- convert back to string
+
+
 code 
 
 def stringRev (word):
-    worLen = len(word)          #getting the lenght of the list
+    worLen = len(word)          #getting the lenght of the list                         
     if worLen == 1:
         return word
     else:
@@ -13,3 +28,7 @@ listWord = sentence.split()         #splitting the string into a list
 output = (stringRev(listWord))      #storing the function into a new varible
 new = ' '.join(output)              #converting the list back into a string
 print(new)
+
+
+#The bigO notation for this algorithm O(n) as if will only through each element of the list and reverse it.
+#Meaning it will only run n number of times. 
